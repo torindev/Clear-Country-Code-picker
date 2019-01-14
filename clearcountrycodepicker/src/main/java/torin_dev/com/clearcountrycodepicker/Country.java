534a38acf126b5cf5f2c23bb891c569ccd145de8
torindev <torindev3@gmail.com>
2019-01-14 17:41:09 +0200

    edits

---
+++ clearcountrycodepicker/src/main/java/torin_dev/com/clearcountrycodepicker/Country.java
@@ -0,0 +1,35 @@
+package torin_dev.com.clearcountrycodepicker;
+
+public class Country {
+  private final String iso;
+  private final String phoneCode;
+  private final String name;
+
+  public Country(String iso, String phoneCode, String name) {
+    this.iso = iso;
+    this.phoneCode = phoneCode;
+    this.name = name;
+  }
+
+  public String getIso() {
+    return iso;
+  }
+
+  public String getPhoneCode() {
+    return phoneCode;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * If country have query word in name or name code or phone code, this will return true.
+   */
+  boolean isEligibleForQuery(String query) {
+    query = query.toLowerCase();
+    return getName().toLowerCase().contains(query)
+        || getIso().toLowerCase().contains(query)
+        || getPhoneCode().toLowerCase().contains(query);
+  }
+}
